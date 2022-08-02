64f85968002429753a6aa4a97e23d6c1ba59d25b
Ruales7 <101646587+Ruales7@users.noreply.github.com>
2022-08-02 11:37:53 -0300

    Add files via upload
    RUALES E LYUAN

---
+++ LY-RUAN/projetoLR1/projetoLR1/Pessoa1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projetoLR1
+{
+    internal class Pessoa1
+    {
+        //Atributo da classe
+        private string nome;
+        private int idade;
+        private double altura;
+        private string Sexo;
+
+        //Construtor sem parâmetro
+        public Pessoa1()
+        {
+            this.nome = null;
+            this.idade = 30;
+            this.altura = 1.75;
+            Sexo = null;
+        }
+
+        //Construtor com parâmetro
+        public Pessoa1(string nome, int idade, double altura, char Sexo)
+        {
+            this.nome = nome;
+            this.idade = idade;
+            this.altura = altura;
+            Sexo = sexo.ToString();
+
+        }
+
+        //Propriedade do nome para acesso ao seu conteúdo
+       public string Nome
+        { 
+            get { return this.nome; } 
+            set { this.nome = value; }
+        }
+        //Método que retorna um string contendo as informações da pessoa
+        public override string ToString()
+        {
+            return(String.Format("Nome: {0}", nome));
+        }
+
+    }
+}
---
+++ LY-RUAN/projetoLR1/projetoLR1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projetoLR1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Criação de um objetro p, do tipo da classe Pessoa1
+            Pessoa1 p = new Pessoa1();
+
+            //Exibindo na tela informaçôes do objeto p,
+            //realizando uma chamada no método ToString
+            Console.WriteLine(p.ToString());
+
+            //Atribuindo um nome ao objetop, por meio da propriedade nome
+            p.Nome = "Neymar";
+
+            //Exibindo na tela informações do objeto p,
+            //após a atribuição do nome
+            Console.WriteLine(p);
+            Console.ReadKey();
+        }
+    }
+}
