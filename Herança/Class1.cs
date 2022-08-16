9dc4b3e182a3e0e3b4ece1830294d7c7502c72fb
Ruales7 <101646587+Ruales7@users.noreply.github.com>
2022-08-16 11:31:00 -0300

    Add files via upload

---
+++ Herança/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herança
+{
+    public abstract class Pessoa
+    {
+        private string nome;
+        private int idade;
+
+        //construtor sem parametro
+        public Pessoa()
+        {
+            this.nome = null;
+            this.idade = 0;
+        }
+        //construtor com parametro
+        public Pessoa(string nome, int idade)
+        {
+            this.nome = nome;
+            this.idade = idade;
+        }
+        //propriedade dos atributos para acesso
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+        public int Idade
+        {
+            get { return idade; }
+            set { idade = value; }
+        }
+
+        //método da classe ImprimeDados
+        public abstract String ImprimeDados();
+    }
+}
